ef651cb7558919583fbb89f73acc42b04bc68ec8
Diego Galíndez <dgalindez1@gmail.com>
2014-03-06 14:49:20 -0600

    Create README.m

---
+++ README.m
@@ -0,0 +1,11 @@
+2-3tree
+=======
+
+My own C++ implementation of a 2-3 Tree
+
+=======
+
+For an overview of the available methods just check tree.h.
+Also, it is not tested yet, let alone completed, so I wouldn't recommend trying it =)
+
+I'll update this when it is.
